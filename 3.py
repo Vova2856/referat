0d0231caac848ccdeebc08ee2ebc25eeea8577d9
Vova2856 <vzehovic@gmail.com>
2025-03-05 09:37:37 +0200

    3

--- 3.py
+++ 3.py
@@ -15,3 +15,37 @@ try:
     penguin.fly()
 except Exception as e:
     print(e)
+
+
+
+
+
+
+
+
+Правильний:
+class Bird:
+    def move(self):
+        pass
+
+class FlyingBird(Bird):
+    def move(self):
+        print("Летить")
+
+class Penguin(Bird):
+    def move(self):
+        print("Плаває")
+
+
+bird = Bird()
+
+
+bird.move()
+
+
+penguin = Penguin()
+penguin.move()
+
+
+flying_bird = FlyingBird()
+flying_bird.move()
