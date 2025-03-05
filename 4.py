89b749d7b199e4af1b4bd47b03cf637bda4ca734
Vova2856 <vzehovic@gmail.com>
2025-03-05 09:43:15 +0200

    4

--- 4.py
+++ 4.py
@@ -24,3 +24,27 @@ try:
     printer.scan()
 except Exception as e:
     print(e)
+
+
+
+
+
+Правильний:
+class Printer:
+    def print(self):
+        pass  # Метод для друку
+
+class Scanner:
+    def scan(self):
+        print("Сканую")
+
+class SimplePrinter(Printer, Scanner):
+    def print(self):
+        print("Друкую")
+
+
+printer = SimplePrinter()
+
+
+printer.print()
+printer.scan()
