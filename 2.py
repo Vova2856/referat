777514c9281a53e6675afd86be7e3ab4884a18c5
Vova2856 <vzehovic@gmail.com>
2025-03-05 09:32:53 +0200

    2

--- 2.py
+++ 2.py
@@ -11,3 +11,26 @@ payment = Payment()
 payment.pay("card")
 payment.pay("paypay")
 payment.pay("cash")
+
+
+
+
+
+Правильний:
+class Payment:
+    def pay(self):
+        pass
+
+class CardPayment(Payment):
+    def pay(self):
+        print("Оплата карткою")
+
+class PayPalPayment(Payment):
+    def pay(self):
+        print("Оплата PayPal")
+
+def process_payment(payment):
+    payment.pay()
+
+process_payment(CardPayment())
+process_payment(PayPalPayment())
