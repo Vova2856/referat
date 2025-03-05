2deff78ac05951f2e3bd9392cd310b9c51150e0b
Vova2856 <vzehovic@gmail.com>
2025-03-05 09:47:56 +0200

    5

--- 5.py
+++ 5.py
@@ -15,3 +15,27 @@ app = App()
 
 
 app.start()
+
+
+
+Правильний:
+class Database:
+    def connect(self):
+        pass
+
+class MySQL(Database):
+    def connect(self):
+        print("Підключення до MySQL")
+
+class App:
+    def __init__(self, db):
+        self.db = db
+
+    def start(self):
+        self.db.connect()
+
+
+app = App(MySQL())
+
+
+app.start()
